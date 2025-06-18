fdd926c51e2606b8390acfe4776aac4205cd7cbe
Ryan Barszcz <rbarszcz@umich.edu>
2025-06-18 15:21:14 -0400

    finished the UI

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 import { Link, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const NavBar = () => {
 
     const {openSignIn} = useClerk()
     const {user} = useUser()
     const navigate = useNavigate()
+    const {setShowRecruiterLogin} = useContext(AppContext)
 
 
   return (
@@ -25,9 +27,8 @@ const NavBar = () => {
                 </div>
                 :
                 <div className='flex gap-4 max-sm:text-xs'>
-                <button className='text-grey-600'>Recruiter Login</button>
-                <button className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'
-                onClick={e => openSignIn()}>Login</button>
+                <button onClick={e => setShowRecruiterLogin(true)} className='text-grey-600'>Recruiter Login</button>
+                <button onClick={e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
             </div>
             }
             
